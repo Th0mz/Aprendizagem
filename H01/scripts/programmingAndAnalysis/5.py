35796f97e5743fead0dbe4679c4aa9fff15785b1
Th0mz <tomastavares21122001@gmail.com>
2021-10-17 10:47:45 +0100

    started programming part

---
+++ H01/scripts/programmingAndAnalysis/5.py
@@ -0,0 +1,9 @@
+import matplotlib
+from scipy.io import arff
+import pandas
+
+data = arff.loadarff("C:\\Users\\tomas\\OneDrive\\Ambiente de Trabalho\\Uni\\3Ano\\1Semestre\\Aprendizagem\\homeworks\\H01\\scripts\\data\\breast.w.arff")
+dataset = pandas.DataFrame(data[0])
+
+print(dataset)
+#usar função hist
