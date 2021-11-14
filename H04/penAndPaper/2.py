80b72b51d85b04500e337484a641d4b1d15e2aa9
Th0mz <tomastavares21122001@gmail.com>
2021-11-14 13:52:03 +0000

    some error fixes to penAndPaper 2

--- H04/penAndPaper/2.py
+++ H04/penAndPaper/2.py
@@ -4,13 +4,13 @@ def euclideanDistance (vector1, vector2):
     return sqrt((vector1[0] - vector2[0])**2 + (vector1[1] - vector2[1])**2)
 
 dataset = [[[2, 4], [-1, 2], [4, 0]], [[-1, -4]]]
+instanceIndex = [1, 3, 4, 2]
 #dataset = [[[0, 0], [1, 0]], [[0, 2], [2, 2]]]
 
 # a(X)
 a = []
 print("a(X)")
 clusterIndex = 1
-instanceIndex = 1
 for cluster in dataset:
     print(f"===== cluster{clusterIndex} =====")
     for i in range(len(cluster)):
@@ -23,14 +23,13 @@ for cluster in dataset:
         else:
             aX = aX / (len(cluster) - 1)
         a.append(aX)
-        print(f"a(x{instanceIndex}) = {aX}")
-        instanceIndex += 1
+        print(f"a(x{instanceIndex[i]}) = {aX}")
     clusterIndex += 1
 
 # b(X)
 b = []
+i = 0 
 print("\nb(X)")
-instanceIndex = 1
 for clusterIndex in range(len(dataset)):
     print(f"===== cluster{clusterIndex + 1} =====")
     for otherClusterIndex in range(len(dataset)):
@@ -43,12 +42,14 @@ for clusterIndex in range(len(dataset)):
                     bX += euclideanDistance(instance, otherInstance)
                 bX = bX / len(otherCluster)
                 b.append(bX)
-                print(f"b(x{instanceIndex}) = {bX}")
-                instanceIndex += 1
+                print(f"b(x{instanceIndex[i]}) = {bX}")
+                i += 1
 
 # S(X)
+i = 0
 print("\nS(X)")
 print("===================")
 for i in range(len(a)):
     sX = 1 - a[i] / b[i]
-    print(f"S(x{i + 1}) = {sX}")
+    print(f"S(x{instanceIndex[i]}) = {sX}")
+    i += 1
